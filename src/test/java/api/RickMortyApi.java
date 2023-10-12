f3caa6012e5ef632114912669e3872771e18c290
VAkhmetov <ahmpht40@gmail.com>
2023-10-12 13:12:21 +0400

    init commit
    Signed-off-by: VAkhmetov <ahmpht40@gmail.com>

--- src/test/java/api/RickMortyApi.java
+++ src/test/java/api/RickMortyApi.java
@@ -5,7 +5,7 @@ import io.restassured.response.Response;
 
 import static api.ApiTemplate.*;
 
-public class RickMortyApi{
+public class RickMortyApi {
     public static String characterLink = "/character/";
     public static String episodeLink = "/episode/";
     public static String charLoc;
@@ -17,50 +17,45 @@ public class RickMortyApi{
     public static String charSpecies;
     public static String charName;
     public static String lastCharName;
-    @Step
+
+    @Step("Получение информации о персонаже")
     public static void rickMorty(String id) {
         Response gettingCharLoc = characterModule(characterLink, id);
         charLoc = optionParseObj(gettingCharLoc, "location", "name");
         charId = optionParse(gettingCharLoc, "id");
         charSpecies = optionParse(gettingCharLoc, "species");
         charName = optionParse(gettingCharLoc, "name");
-
-        System.out.println("Персонаж: " + charName);
-        System.out.println("id: " + charId);
-        System.out.println("Вид: " + charSpecies);
-        System.out.println("Локация: " + charLoc);
     }
-    @Step
+
+    @Step("Выбор последнего эпизода, где появлялся персонаж")
     public static void gettingLastEpisode() {
         Response gettingLastEpisode = characterModule(characterLink, charId);
 
         int episode = optionParseArray(gettingLastEpisode, "episode");
         lastEpisode = optionParseString(gettingLastEpisode, "episode", episode);
     }
-    @Step
+
+    @Step("Получение id последнего персонажа последней локации последнего эпизода, где появлялся персонаж")
     public static void gettingCharLastEpisode() {
         Response gettingCharLastEpisode = characterModule(episodeLink, lastEpisode);
         int character = optionParseArray(gettingCharLastEpisode, "characters");
         lastChar = optionParseString(gettingCharLastEpisode, "characters", character);
-
-        System.out.println("id последнего персонажа последней локации последнего эпизода, где появлялся " + charName + ": " + lastChar);
     }
-    @Step
+
+    @Step("Получение информации о полученном персонаже из последнего эпизода, где появлялся персонаж из первого шага")
     public static void gettingCharInfoLastEpisode() {
         Response gettingCharInfoLastEpisode = characterModule(characterLink, lastChar);
         lastCharName = optionParse(gettingCharInfoLastEpisode, "name");
         lastCharSpecies = optionParse(gettingCharInfoLastEpisode, "species");
         lastCharLocation = optionParseObj(gettingCharInfoLastEpisode, "location", "name");
-
-        System.out.println("Имя данного персонажа: " + lastCharName);
-        System.out.println("Вид данного персонажа: " + lastCharSpecies);
-        System.out.println("Локация данного персонажа: " + lastCharLocation);
     }
-    @Step
+
+    @Step("Проверка локаций первого и второго персонажей")
     public static void speciesCheck(String charSpecies, String lastCharSpecies) {
         testParams(charSpecies, lastCharSpecies, "Вид не совпадает");
     }
-    @Step
+
+    @Step("Проверка вида первого и второго персонажей")
     public static void locCheck(String charLoc, String lastCharLocation) {
         testParams(charLoc, lastCharLocation, "Локации не совпадают");
     }
